a6ee87b9a7db314b8aea6a1b4aebf8a8fef9b8b9
zonghuaj <8163944@qq.com>
2020-06-09 10:34:21 +0800

    first commit

---
+++ build/release.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+set -e
+
+git checkout master
+git merge dev
+
+VERSION=`npx select-version-cli`
+
+read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Releasing $VERSION ..."
+
+  # build
+  VERSION=$VERSION npm run dist
+
+  # ssr test
+  node test/ssr/require.test.js            
+
+  
+
+  if [[ $VERSION =~ "beta" ]]
+  then
+    npm publish --tag beta
+  else
+    npm publish
+  fi
+fi
